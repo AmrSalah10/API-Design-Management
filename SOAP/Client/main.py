90716d6fabd19e334db13e8389dd9b3cd6b6f6d2
unknown <amr.thefirst2010@yahoo.com>
2026-02-08 03:17:12 +0200

    feat: Implement initial Zeep SOAP client for the calculator.

--- SOAP/Client/main.py
+++ SOAP/Client/main.py
@@ -2,7 +2,7 @@ from zeep import Client
 
 def main():
     client = Client(wsdl="http://localhost:8080/calc?wsdl")
-    print(client)
+    print(client.service.Add(15, 30))
 
 if __name__ == "__main__":
     main()
